075c574bf0634235b0b4ed4459a2aaf2475beac0
Udo Sell Júnior <82981126+selludojunior@users.noreply.github.com>
2023-03-28 09:48:03 -0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,61 @@
+# <img src="./web/src/assets/logo.svg" height=25 alt="logo nlw Copa"> Trilha Ignite | Rocketseat
+
+### :soccer: Project Copa
+![alt text](https://i.imgur.com/SCwHfwF.png) ![alt text](https://i.imgur.com/OVnqdXn.png)
+
+# :rocket: Developed Using: </br>
+* React
+* React-Native
+* Expo
+* TypeScript
+* Fastify
+* Axios
+* Google OAuth
+* Prisma
+
+# :heavy_check_mark: Run Project:
+
+#### --Server :globe_with_meridians:
+```bash
+cd server
+```
+
+```bash
+npm install
+```
+
+```bash
+npm run dev
+```
+
+#### --Web :computer:
+```bash
+cd web
+```
+
+```bash
+npm install
+```
+
+```bash
+npm run dev
+```
+
+#### --Mobile :iphone:
+
+```bash
+cd mobile
+```
+
+```bash
+npm install
+```
+
+```bash
+npx expo start
+```
+
+### Important :heavy_exclamation_mark:
+```bash
+Change .env.example to .env and fill the CLIENT_ID variable with your Google client id
+```
---
+++ mobile/App.tsx
@@ -0,0 +1,34 @@
+import { NativeBaseProvider, StatusBar } from "native-base"
+import { Loading } from "./src/components/Loading"
+import { THEME } from "./src/styles/theme"
+import React from "react";
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_500Medium,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto"
+import { AuthContextProvider } from "./src/context/AuthContext";
+
+import { Routes } from "./src/routes";
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_500Medium,
+    Roboto_700Bold,
+  })
+
+  return (
+    <NativeBaseProvider theme={THEME}>
+      <AuthContextProvider>
+        <StatusBar
+          barStyle="light-content"
+          backgroundColor="transparent"
+          translucent
+        />
+        {fontsLoaded ? <Routes /> : <Loading />}
+      </AuthContextProvider>
+    </NativeBaseProvider>
+  )
+}
